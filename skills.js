e9e08ea7f3d8cb48a727420a4f2dd9519dae41fd
MomirMilutinovic <40830508+MomirMilutinovic@users.noreply.github.com>
2023-07-19 13:34:39 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,8 @@
+function calculateNumbers(var1, var2) {
+    const sum = var1 + var2;
+    const diff = var1 - var2;
+    const mult = var1 * var2;
+    const div = var1 / var2;
+    const result = `Sum: ${sum}, Diff: ${diff}, Mult: ${mult}, Div: ${div}`;
+    return result;
+}
